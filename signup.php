790f3563dc266891c53f16af1892aae31b5772c7
Harsha <harshabm16@gmail.com>
2025-04-15 19:04:18 +0530

    jj1

---
+++ signup.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+</head>
+<body>
+    <h2>Signup Page Coming Soon!</h2>
+    <p><a href="login.php">Back to Login</a></p>
+</body>
+</html>
